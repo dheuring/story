62fccd789b2bfa666b016dfbafc4f15d08081b69
dheuring <dheuring@users.noreply.github.com>
2026-01-17 13:44:50 -0600

    Delete function/[[path]].js

--- function/[[path]].js
+++
@@ -1,9 +0,0 @@
-export async function onRequest(context) {
-  const url = new URL(context.request.url);
-
-  if (url.pathname === '/' || url.pathname === '/index.html') {
-    return Response.redirect(`${url.origin}/home.html`, 302);
-  }
-
-  return context.next();
-}
